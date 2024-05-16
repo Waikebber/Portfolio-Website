3167d86868f58447b8b7d4c551198d63f281333c
Waikebber <waikebber@gmail.com>
2024-05-15 22:09:23 -0400

    added projects and overlays

--- js/project-overlays.js
+++ js/project-overlays.js
@@ -1,7 +1,7 @@
 function showOverlay(title, subtitle, description, imageUrl) {
     document.getElementById("overlay-title").innerText = title;
     document.getElementById("overlay-subtitle").innerText = subtitle;
-    document.getElementById("overlay-description").innerText = description;
+    document.getElementById("overlay-description").innerHTML = description;
     document.getElementById("overlay-image").src = imageUrl;
     document.getElementById("overlay").style.display = "flex";
 }
