38e54af3eb8a3ec15518daef2a24cb52e1d3aeba
Waikebber <waikebber@gmail.com>
2024-05-20 18:57:23 -0400

    added excel package

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+const xlsx = require('xlsx');
 
 const app = express();
 const PORT = 3000;
 
+// Serve static files from the public directory
 app.use(express.static('public'));
 
+// Endpoint to fetch images and locations from the Excel file
 app.get('/images', (req, res) => {
-    const imagesDir = path.join(__dirname, 'public/images/photography');
-    fs.readdir(imagesDir, (err, files) => {
-        if (err) {
-            return res.status(500).json({ error: 'Unable to scan directory' });
-        }
-        const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
-        const imagePaths = imageFiles.map(file => `/images/photography/${file}`);
-        res.json(imagePaths);
-    });
+    const workbook = xlsx.readFile(path.join(__dirname, 'images.xlsx'));
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const data = xlsx.utils.sheet_to_json(worksheet);
+    res.json(data);
 });
 
 app.listen(PORT, () => {
