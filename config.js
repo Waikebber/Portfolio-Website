6e50cbcb3a5fe8a841c279a62edf540e477165e5
Waikebber <waikebber@gmail.com>
2024-05-22 17:09:41 -0400

    added placeholder config file

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    myEmail: 'my-email@---.com',
+    fromEmail: 'website-email@---.com',
+    fromEmailPassword: 'website-email-password',
+    mySQLUser: 'mysql-username',
+    mySQLPassword: 'mysql-password',
+    databaseName: 'database-name'
+};
