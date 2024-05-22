4cefe5332f3ef33a977467894ff0d48a8295743e
Waikebber <waikebber@gmail.com>
2024-05-22 18:39:32 -0400

    changeed emailing method

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    myEmail: 'my-email@---.com',
-    fromEmail: 'website-email@---.com',
-    fromEmailPassword: 'website-email-password',
-    mySQLUser: 'mysql-username',
-    mySQLPassword: 'mysql-password',
-    databaseName: 'database-name'
-};
--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
-const nodemailer = require('nodemailer');
 const fs = require('fs');
 const path = require('path');
 const xlsx = require('xlsx');
-
-// Import configuration
-let config;
-if (fs.existsSync('config.local.js')) {
-    config = require('./config.local.js');
-} else {
-    config = require('./config.js');
-}
-
-module.exports = config;
 
 const app = express();
 const PORT = 3000;
@@ -25,61 +13,6 @@ app.use(bodyParser.json());
 
 // Serve static files
 app.use(express.static('public'));
-
-// MySQL connection
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: config.mySQLUser,
-    password: config.mySQLPassword,
-    database: config.databaseName
-});
-
-db.connect(err => {
-    if (err) throw err;
-    console.log('MySQL connected...');
-});
-
-// Nodemailer setup
-const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: config.fromEmail,
-        pass: config.fromEmailPassword
-    }
-});
-
-// POST route to handle form submission
-app.post('/contact', (req, res) => {
-    const { fName, lName, email, sub, message } = req.body;
-
-    // Insert data into the database
-    const query = 'INSERT INTO contacts (first_name, last_name, email, subject, message) VALUES (?, ?, ?, ?, ?)';
-    db.execute(query, [fName, lName, email, sub, message], (err, result) => {
-        if (err) {
-            console.error('Database error:', err);
-            res.status(500).send('Server error');
-            return;
-        }
-
-        // Send confirmation email
-        const mailOptions = {
-            from: config.fromEmail,
-            to: config.myEmail,
-            subject: `New Contact Form Submission from ${fName} ${lName}`,
-            text: `You have received a new message from ${fName} ${lName}.\n\nEmail: ${email}\n\nSubject: ${sub}\n\nMessage:\n${message}`,
-            cc: email
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error('Email error:', error);
-                res.status(500).send('Server error');
-                return;
-            }
-            res.send('Message sent successfully!');
-        });
-    });
-});
 
 // Endpoint to fetch images and locations from the Excel file
 app.get('/images', (req, res) => {
