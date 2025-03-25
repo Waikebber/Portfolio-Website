5144dcf6d3bdf6fbd55a515b2201f11eac89f153
Waikebber <waikebber@gmail.com>
2025-03-24 22:39:22 -0400

    added in the resumes section

---
+++ client/src/components/buffers/ResumeToPhotoBuffer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FullWidthImage from './FullWidthImage';
+
+const ResumeToPhotoBuffer: React.FC = () => {
+    return (
+        <FullWidthImage
+            src="/assets/buffers/resume-photo-buffer.svg"
+            alt="Hero to Project Buffer"
+        />
+    );
+};
+
+export default ResumeToPhotoBuffer;
