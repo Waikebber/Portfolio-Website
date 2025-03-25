097156cf4b0b57e5850d296f5ce45ede3201db15
Waikebber <waikebber@gmail.com>
2025-03-24 21:25:44 -0400

    Fixed metadata and icon

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,8 @@
 export const metadata = {
   title: "Kai Webber Portfolio",
-  description: "...",
+  description: "Kai Webber's personal website.",
+  keywords: "Kai Webber, Northeastern University, NEU, Computer Science, Computer Engineering, Portfolio, Kai, Webber, Student",
+  author: "Kai Webber",
 };
 
 export default function RootLayout({
@@ -10,6 +12,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="author" content={metadata.author} />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon_io/favicon-16x16.png" />
+        <link rel="manifest" href="/assets/favicon_io/site.webmanifest" />
+      </head>
       <body>{children}</body>
     </html>
   );
