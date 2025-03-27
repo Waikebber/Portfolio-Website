e66c3c9e06bf479cc6054dc36e7735bf0aeaa525
Waikebber <waikebber@gmail.com>
2025-03-27 02:07:54 -0400

    Added mounting check

--- client/src/components/main/HeroSection/HeroSection.tsx
+++ client/src/components/main/HeroSection/HeroSection.tsx
@@ -1,14 +1,23 @@
 'use client';
+
 import { useMediaQuery } from "@chakra-ui/react";
 import HeroFullWidth from "./HeroFullWidth/HeroFullWidth";
 import HeroCompact from "./HeroCompact/HeroCompact";
+import { useEffect, useState } from "react";
 import "@/styles/global.css";
 
 const HeroSection = () => {
+  const [isClient, setIsClient] = useState(false);
   const [isCompact] = useMediaQuery(
     ["(max-width: 900px)", "(orientation: portrait)"],
     { ssr: false }
   );
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
 
   return isCompact ? <HeroCompact /> : <HeroFullWidth />;
 };
